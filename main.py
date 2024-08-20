61918457e756e6fc03f9383ddc12a408ced3f534
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-20 20:32:55 +0530

    add

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ import random
 
 options = ['r','p','s']
 options_inOBj = {'r':"Rock",'p':'Paper','s':'Scissor'}
+score_file = open('score.txt','r+w')
+score = score_file.read()
 shoot = input("Enter a KEY: ").lower();
 
-samantha = random.choice(options)
+samantha = random.choice(options);
 
 print(f'Your shoot {options_inOBj.get(shoot)} and samantha shoot {options_inOBj.get(samantha)}',end=": ")
 
@@ -21,14 +23,21 @@ else:
     if shoot == 'r' and samantha == 'p':
         print('Samantha win 🥳')
     elif shoot=='p' and samantha == 'r':
+        score += 1;
         print('You Win 🥳 🚀');
     elif shoot == 's' and samantha == 'p':
+        score += 1;
         print("You win 🥳🚀");
     elif shoot == 'p' and samantha == 's':
          print('Samantha win 🥳');
     elif shoot == 'r' and samantha == 's':
+        score += 1;
         print("You win 🥳🚀");
     elif shoot=='s' and samantha == 'r':
         print('Samantha win 🥳');
     else:
         print("Someone did bad something 🪨  📜 ✂️")
+        
+print(score)
+score_file.write(score)
+score_file.close()
